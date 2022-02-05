2f4efba9f9247f17a744b8ad62d30a1a195cb047
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 12:12:42 +0530

    Task56

--- src/script.js
+++ src/script.js
@@ -333,3 +333,12 @@ function addFive() {
 
 addThree();
 addFive();
+//Task56
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
