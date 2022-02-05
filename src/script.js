907ea63e6b4917602fdac6dcc05f25fe6ce6e81d
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 12:42:16 +0530

    Task65

--- src/script.js
+++ src/script.js
@@ -421,3 +421,19 @@ function testStrictNotEqual(val) {
 }
 
 testStrictNotEqual(10);
+//Task65
+function testGreaterThan(val) {
+  if (val > 100) {
+    // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
