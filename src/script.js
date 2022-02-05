94585d42f69a15214eb1f7e75377c5d27d6af2dd
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 12:05:22 +0530

    Task52

--- src/script.js
+++ src/script.js
@@ -271,7 +271,27 @@ function functionWithArgs(a, b) {
 }
 functionWithArgs(2, 6);
 //Task51
-function timesFive(a){
-    return 5*a;
-  
+function timesFive(a) {
+  return 5 * a;
+}
+//Task52
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
   }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
