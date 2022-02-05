ee2ff6f835d7922a5b61f42b0be39a50f7bce329
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 13:07:33 +0530

    Task74

--- src/script.js
+++ src/script.js
@@ -548,3 +548,23 @@ function orderMyLogic(val) {
 }
 
 orderMyLogic(7);
+//Task 74
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
