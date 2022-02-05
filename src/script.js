9f9a950ec0fa61543ddabc134a20036c720d7b10
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 11:32:33 +0530

    Task36

--- src/script.js
+++ src/script.js
@@ -169,3 +169,9 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//Task36
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
