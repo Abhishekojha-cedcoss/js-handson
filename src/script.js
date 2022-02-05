01d5312fc2e0e883e552d6d6c1aa9a50c1ea9806
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 11:31:12 +0530

    Task35

--- src/script.js
+++ src/script.js
@@ -163,3 +163,9 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr="Hello World"; // Change this line
 // Only change code above this line
+
+//Task35
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
