8ca579242ee8e0377e4a6fa8198d6006755944a0
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 13:52:33 +0530

    Task86

--- src/script.js
+++ src/script.js
@@ -788,3 +788,13 @@ const testObj = {
 // Only change code below this line
 const entreeValue = testObj["an entree"]; // Change this line
 const drinkValue = testObj["the drink"]; // Change this line
+//Task 86
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas",
+};
+
+// Only change code below this line
+const playerNumber = 16; // Change this line
+const player = testObj[playerNumber]; // Change this line
