1c5450d39d8db1d63f7851d4a7dc3e09c8fe32bd
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 12:34:35 +0530

    Task60

--- src/script.js
+++ src/script.js
@@ -371,3 +371,13 @@ function trueOrFalse(wasThatTrue) {
   }
   return "No, that was false";
 }
+//Task60
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
