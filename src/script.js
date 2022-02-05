dab11b4a66d31631b94b7a318e4e8c9e86957cbd
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 12:51:11 +0530

    Task70

--- src/script.js
+++ src/script.js
@@ -497,3 +497,16 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+//Task70
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
