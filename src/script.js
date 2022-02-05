1a4b589687d9dcaf038485efe6c52e20ccd83ce3
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 11:11:47 +0530

    Task27

--- src/script.js
+++ src/script.js
@@ -120,3 +120,5 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Ch
 
 //Task26
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+//Task27
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
