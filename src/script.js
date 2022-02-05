5798b21cfbfcd09bba07015c1adab22e9ffa2407
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 10:46:06 +0530

    Task10

--- src/script.js
+++ src/script.js
@@ -46,3 +46,5 @@ const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
+//Task11
+const sum = 10 + 10;
