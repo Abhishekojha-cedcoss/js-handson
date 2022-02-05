6cb5869de2bb0d5f6c12fd402317878a5f116fe4
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 13:26:01 +0530

    Task77

--- src/script.js
+++ src/script.js
@@ -627,3 +627,27 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
+//Task77
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
