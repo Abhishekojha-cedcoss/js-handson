27029801a3147b0a759df87a03551e00b13605e4
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 10:36:14 +0530

    Task7

--- src/script.js
+++ src/script.js
@@ -18,3 +18,13 @@ var a = 9;
 //Task6
 var myFirstName="Abhishek";
 var myLastName="Ojha";
+//Task7
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
