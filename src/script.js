da5536d7b0ec2491e1275b2d86175d8d25daa58e
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 11:47:13 +0530

    Task44

--- src/script.js
+++ src/script.js
@@ -213,4 +213,8 @@ const myArray = [
   ];
   
   const myData = myArray[2][1];
-  
+//Task44
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog",3]);
