ec4c595c9866c7b644015d4a3d512566c618551b
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 12:37:35 +0530

    Task62

--- src/script.js
+++ src/script.js
@@ -391,3 +391,13 @@ function testStrict(val) {
 }
 
 testStrict(10);
+//Task62
+function compareEquality(a, b) {
+  if (a === b) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
