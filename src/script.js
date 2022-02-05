a64c2b4def715a83c48f73426ac143d149497842
Abhishek <abhishekojha@cedcoss.com>
2022-02-05 10:38:55 +0530

    Task8

--- src/script.js
+++ src/script.js
@@ -28,3 +28,13 @@ var c = "I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+//Task8
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
