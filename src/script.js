366d5d0eb0c7351571e235ec03945de8d30045b3
Abhishek Ojha <98630436+Abhishekojha-cedcoss@users.noreply.github.com>
2022-02-07 14:14:30 +0530

    Iterate with while loop

--- src/script.js
+++ src/script.js
@@ -960,3 +960,47 @@ const myPlants = [
 
   const gloveBoxContents = myStorage.car.inside["glove box"]; 
   const secondTree = myPlants[1].list[1]; 
+//Task96
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+   if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value
+  }
+  else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value]
+  }
+  else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value)
+  }
+  else if (value === "") {
+    delete records[id][prop]
+  }
+  return records;
+const myArray = [];
+var a = 5
+while (a >= 0) {
+  myArray.push(a)
+  a--
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA'); 
